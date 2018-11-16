e1ef38bc81a39ad86324fd5e794900efe088ccf6
Peter Blum <pblum@pivotal.io>
2018-11-15 22:38:52 -0600

    echo namespace into config map

--- kubernetes/tasks/create-prometheus-config-map/task.sh
+++ kubernetes/tasks/create-prometheus-config-map/task.sh
@@ -4,6 +4,8 @@ set -xu
 
 cp kube-config/config ~/.kube/config
 
+echo "  namespace: $K8S_NAMESPACE" >> pks-prometheus/configMap.yml
+
 kubectl apply -f pks-prometheus/configMap.yml -n $K8S_NAMESPACE
 
 kubectl get configmaps -n $K8S_NAMESPACE
