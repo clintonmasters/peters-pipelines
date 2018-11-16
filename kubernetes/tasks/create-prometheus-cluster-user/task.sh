e2819ab7c263c53f15bc33ef00a83c9c5b88e656
Peter Blum <pblum@pivotal.io>
2018-11-15 22:28:11 -0600

    create -> apply

--- kubernetes/tasks/create-prometheus-cluster-user/task.sh
+++ kubernetes/tasks/create-prometheus-cluster-user/task.sh
@@ -4,7 +4,7 @@ set -xu
 
 cp kube-config/config ~/.kube/config
 
-kubectl create -f pks-prometheus/clusterRole.yml -n $K8S_NAMESPACE
+kubectl apply -f pks-prometheus/clusterRole.yml -n $K8S_NAMESPACE
 
 kubectl get clusterroles -n $K8S_NAMESPACE
 
