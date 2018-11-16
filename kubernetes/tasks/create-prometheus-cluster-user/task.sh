9a4ce772e5cf2006dd60d04d09b3bfb51f6918ac
Peter Blum <pblum@pivotal.io>
2018-11-15 22:45:30 -0600

    echo namespace into cluster role

--- kubernetes/tasks/create-prometheus-cluster-user/task.sh
+++ kubernetes/tasks/create-prometheus-cluster-user/task.sh
@@ -4,6 +4,8 @@ set -xu
 
 cp kube-config/config ~/.kube/config
 
+echo "  namespace: $K8S_NAMESPACE" >> pks-prometheus/clusterRole.yml
+
 kubectl apply -f pks-prometheus/clusterRole.yml -n $K8S_NAMESPACE
 
 kubectl get clusterroles -n $K8S_NAMESPACE
