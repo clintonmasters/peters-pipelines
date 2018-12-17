cdbf8dbb8752b51b306882cdf6022208380ef238
Clinton Masters <10438162+clintonmasters@users.noreply.github.com>
2018-12-16 21:45:59 -0500

    pwd to full helm options

--- kubernetes/tasks/install-helm-chart/task.sh
+++ kubernetes/tasks/install-helm-chart/task.sh
@@ -11,12 +11,10 @@ if [[ -x /helm ]]; then
   export PATH=/:$PATH
 fi
 
-echo $PWD
-
 mkdir -m 700 -p ~/.kube
 cp kube-config/config ~/.kube/config
 
-#helm init --client-only
+helm init --client-only
 
 # Set chart values from file if CHART_VALUES exists
 FILE=${CHART_VALUES:-novalues}
@@ -24,10 +22,10 @@ FILE=${CHART_VALUES:-novalues}
 if [[ "$FILE" == "novalues" ]]; then
   HELM_OPTIONS=""
 else
-  HELM_OPTIONS="--values \"$CHART_VALUES\""
+  HELM_OPTIONS="--values \"$PWD/$CHART_VALUES\""
 fi
 
 # Install the Chart
-#helm upgrade $RELEASE_NAME stable/$CHART_NAME --install $HELM_OPTIONS
+helm upgrade $RELEASE_NAME stable/$CHART_NAME --install $HELM_OPTIONS
 
 
